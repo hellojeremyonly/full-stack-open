e0cec65afdbed9e33d0790430b10af1d1a85d807
Jeremy <JeremyC.dev@proton.me>
2025-11-29 21:47:10 +0800

    Added exercises 1.2

--- part1/courseinfo/1.2: Course Information/src/App.jsx
+++ part1/courseinfo/1.2: Course Information/src/App.jsx
@@ -9,9 +9,17 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
-      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
-      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+        <p>{props.part.name} {props.part.exercises}</p>
     </div>
   )
 }
